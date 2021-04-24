b359803409c625f1d7a53d8d123ffdd036c94228
ibrahimmoamen100 <ibrahim.moamen100@gmail.com>
2021-04-24 03:53:34 +0200

    style create comment section

--- src/Components/Sections/CreateComment.js
+++ src/Components/Sections/CreateComment.js
@@ -2,19 +2,37 @@ import React from "react";
 import "./CreateComment.css";
 import user from "../../image/cc.jpg";
 import "../../App.css";
+import { render } from "@testing-library/react";
 
-export default function CreateComment() {
-  return (
-    <div className="create-comment">
-      <div className="img-inpt">
-        <div className="user-img">
-          <img src={user} />
+export default class CreateComment extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="create-comment">
+        <div className="img-inpt">
+          <div className="user-img">
+            <img src={user} />
+          </div>
+          <textarea
+            type="text"
+            placeHolder="What is on your mind , Ahmed?"
+          ></textarea>
         </div>
-        <textarea
-          type="text"
-          placeHolder="What is on your mind , Ahmed?"
-        ></textarea>
-      </div>
-    </div>
-  );
+        <div className="image-post">
+          <div className="img-btn">
+            <input name="img-vid" id="img-vid" type="file" />
+            <label className="btn " name="img-vid" for="img-vid">
+              <i class="far fa-images"></i>
+              <span> Photo/Video </span>
+            </label>
+          </div>
+          <button className="btn btn-primary" type="submit">
+            Post
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
