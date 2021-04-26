e109c56b7a144c8252aa57bfc4be1bd37ec31024
ibrahimmoamen100 <ibrahim.moamen100@gmail.com>
2021-04-26 05:40:11 +0200

    new comment posted

--- src/Components/Sections/CommentPosted.js
+++ src/Components/Sections/CommentPosted.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./CommentPosted.css";
 import user from "../../image/cc.jpg";
+import like from "../../image/like.svg";
+import angry from "../../image/angry.svg";
+import haha from "../../image/haha.svg";
+import wow from "../../image/wow.svg";
+import care from "../../image/care.svg";
 import { NavLink } from "react-router-dom";
+import ReactTooltip from "react-tooltip";
 
 export default function CommentPosted() {
   return (
@@ -22,7 +28,25 @@ export default function CommentPosted() {
         </div>
       </div>
       <div className="comment-content">
-        <span>lorem ipsum dolor sit amet, consectetur adip</span>
+        <span>
+          lorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit
+          amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip
+          lorem ipsum dolor sit amet, consectetur adip
+        </span>
+        <img src={user} />
+      </div>
+      <div className="like-comment">
+        <div className="likes" data-tip data-for="like">
+          <img src={like} />
+          <img src={angry} />
+
+          <ReactTooltip id="like" place="bottom" effect="solid">
+            Ahmed Hamdy <br></br> Ahmed Hamdy <br></br> Ahmed Hamdy <br></br>
+            Ahmed Hamdy <br></br> Ahmed Hamdy
+          </ReactTooltip>
+          <span style={({ color: "gray" }, { marginLeft: "3px" })}> 5 </span>
+        </div>
+        <span> 17 Comments </span>
       </div>
     </div>
   );
